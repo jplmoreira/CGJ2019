06c642dfdf507d282fb9c031b1cb6444fe20b97e
jplmoreira <jplmoreira92@gmail.com>
2019-09-27 16:54:54 +0100

    Add abstract class of GL applications

---
+++ src/GLengine.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "i_GLapp.h"
+
+class GLengine {
+	static GLengine* instance;
+
+	GLengine() {}
+
+public:
+	static GLengine* get_instance() {
+		if(!instance) instance = new GLengine();
+		return instance;
+	}
+
+	GLFWwindow* setup(i_GLapp* app, int major, int minor,
+		int winx, int winy, const char* title, int is_fullscreen, int is_vsync);
+
+	void run(GLFWwindow* win);
+};
